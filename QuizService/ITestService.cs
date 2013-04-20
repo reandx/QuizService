c314dec148fec53d867d0f928f32ba33ae5c367f
retalzie <citak.alper@gmail.com>
2013-04-20 20:16:32 +0300

    Initial commit with test service

---
+++ QuizService/ITestService.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace QuizService
+{
+    [ServiceContract]
+    public interface ITestService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/add?v1={value1}&v2={value2}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        int Add(int value1, int value2);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/multiply?v1={value1}&v2={value2}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        int Multiply(int value1, int value2);
+    }//interface
+}//namespace
+    
---
+++ QuizService/TestService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuizService
+{
+    public class TestService : ITestService
+    {
+        #region "Methods"
+
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }//function
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }//function
+
+        #endregion
+    }//class
+}//namespace
